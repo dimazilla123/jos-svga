70554648b335aaf8df09730659ef8d9cb028206a
Evtushenko Dmitry <evtushenko.2002@mail.ru>
2021-11-04 13:41:32 +0300

    added "Hello World!" for additional question

--- prog/test1.c
+++ prog/test1.c
@@ -1,12 +1,15 @@
 void (*volatile sys_yield)(void);
+void (*volatile hello)(void);
 
 void
 umain(int argc, char **argv) {
     int i, j;
 
+    
     for (j = 0; j < 3; ++j) {
         for (i = 0; i < 10000; ++i)
             ;
+        hello();
         sys_yield();
     }
 }
